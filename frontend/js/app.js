a7195fb21d782fdfd036489ee063b10935bb89cb
郝广博 <haoguangbo@sparker.xyz>
2016-04-06 17:45:16 +0800

    任务板的panel完成

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -33,16 +33,51 @@ app.run(['$rootScope', '$window', '$location', '$log', function ($rootScope, $wi
 
     $rootScope.groups=[
         {
-            groupname:'分组1',
+            groupname:'主要任务',
             tasks:[
                 {
-                    title:'task1',
-                    type:'normal',
+                    title:'完善通讯录',
+                    type:'正常',
                     remark:'some word here',
                     tasker_main:'henry',
                     tasker_other:['jim','john'],
-                    participator:['1','2','3'],
+                    participators:['1','2','3'],
                     publisher:'henry',
+                    ddl:'1288323623006'
+                },
+                {
+                    title:'task2',
+                    type:'正常',
+                    remark:'',
+                    tasker_main:'12',
+                    tasker_other:['小李','小王'],
+                    participators:['1','2','3'],
+                    publisher:'小华',
+                    ddl:'1289355623006'
+                },
+                {
+                    title:'紧急会议',
+                    type:'紧急',
+                    remark:'some word here',
+                    tasker_main:'henry',
+                    tasker_other:['smith','harry'],
+                    participators:['1','2','3'],
+                    publisher:'小明',
+                    ddl:'1285978923006'
+                }
+            ]
+        },
+        {
+            groupname:'技术任务',
+            tasks:[
+                {
+                    title:'task1',
+                    type:'火急',
+                    remark:'some word here',
+                    tasker_main:'henry',
+                    tasker_other:['jim','john'],
+                    participators:['1','2','3'],
+                    publisher:'mary',
                     ddl:'2016-4-3'
                 }
             ]
