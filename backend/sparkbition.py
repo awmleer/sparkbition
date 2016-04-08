f4c8e3a493020ca7dbabc931beffc5374d8afd94
dxc4598 <dxc4598@163.com>
2016-04-08 10:38:13 +0800

    init

--- backend/sparkbition.py
+++ backend/sparkbition.py
@@ -1,19 +1,47 @@
-from flask import Flask
-from flask import request
-from flask.ext.cors import CORS    #跨域请求
+# coding:utf-8
+
+from flask import Flask, request, make_response
+import pymongo
+import json
+import datetime
+from pymongo import MongoClient
+from flask.ext.cors import CORS   #跨域访问
 
 app = Flask(__name__)
-CORS(app)
+CORS(app)   #跨域访问
+
+#登录及用户认证
+client = MongoClient('120.27.123.112', 27017)
+client.admin.authenticate('fqs', '123456', mechanism='MONGODB-CR')
+uri = "mongodb://fqs:123456@120.27.123.112/admin?authMechanism=MONGODB-CR"
+client = MongoClient(uri)
+
+db = client['tasks']
+coll = db['groups']
+
+# for item in coll.find():
+#     print(item)
+
+@app.route('/sparkbition/api/task')
+def task():
+    for item in coll.find():
+        ff = json.dumps(item)
+        return ff
 
 
-@app.route('/api/login')
-def login():
-    resp = make_response(render_template(...))
-    resp.set_cookie('username', 'the username')
-    return resp
+# @app.route('/api/login')
+# def login():
+#     resp = make_response('success',200)
+#     resp.set_cookie('username', 'the username')
+#     return resp
 
-
-
+# @app.route('/api/func1')
+# def func1():
+#     username = request.cookies.get('username')
+#     resp = make_response('failed',200)
+#     if username == 'the username':
+#         resp = make_response('success',200)
+#     return resp
 
 #@app.route('/new')
 #def new():
