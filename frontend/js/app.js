a5b00dc16723427aed020ddc23b27a56c0e5e44d
haoguangbo <haoguangbo@yeah.net>
2016-04-22 22:20:45 +0800

    各种API对接

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -19,6 +19,10 @@ app.config(function($stateProvider, $urlRouterProvider){
             url: "/message",
             templateUrl: "partials/message.html"
         })
+        .state('statistics', {
+            url: "/statistics",
+            templateUrl: "partials/statistics.html"
+        })
         .state('state2', {
             url: "/state2",
             templateUrl: "partials/state2.html"
@@ -44,78 +48,78 @@ app.run(['$rootScope', '$window', '$location', '$log', function ($rootScope, $wi
     
 
     
-    $rootScope.groups=[
-        {
-            groupname:'主要任务',
-            index:0,
-            tasks:[
-                {
-                    id:2,
-                    title:'完善通讯录aaweeeeg',
-                    urgency:'正常',
-                    remark:'some word here',
-                    tasker_main:'henry',
-                    tasker_other:['jim','john'],
-                    participators:['1','2','3'],
-                    publisher:'henry',
-                    ddl:'1288323623006',
-                    finishtime:'',
-                    base_score:'5',
-                    upvoters:['jim','小华'],
-                    status:0
-                },
-                {
-                    id:3,
-                    title:'task2',
-                    urgency:'正常',
-                    remark:'',
-                    tasker_main:'12',
-                    tasker_other:['小李','小王'],
-                    participators:['1','2','3'],
-                    publisher:'小华',
-                    ddl:'1289355623006',
-                    finishtime:'1288323623006',
-                    base_score:3,
-                    upvoters:[],
-                    status:2
-                },
-                {
-                    id:4,
-                    title:'紧急会议',
-                    urgency:'紧急',
-                    remark:'some word here',
-                    tasker_main:'henry',
-                    tasker_other:['smith','harry'],
-                    participators:['1','2','3'],
-                    publisher:'小明',
-                    ddl:'1461180991000',
-                    finishtime:'',
-                    base_score:2,
-                    upvoters:['henry'],
-                    status:0
-                }
-            ]
-        },
-        {
-            groupname:'技术任务',
-            index:1,
-            tasks:[
-                {
-                    id:5,
-                    title:'task1',
-                    urgency:'火急',
-                    remark:'some word here',
-                    tasker_main:'henry',
-                    tasker_other:['jim','john'],
-                    participators:['1','2','3'],
-                    publisher:'mary',
-                    ddl:'1285978923006',
-                    finishtime:'1288323623006',
-                    base_score:'5',
-                    upvoters:['jim','小华'],
-                    status:1
-                }
-            ]
-        }
-    ]
+    // $rootScope.groups=[
+    //     {
+    //         groupname:'主要任务',
+    //         index:0,
+    //         tasks:[
+    //             {
+    //                 id:2,
+    //                 title:'完善通讯录aaweeeeg',
+    //                 urgency:'正常',
+    //                 remark:'some word here',
+    //                 tasker_main:'henry',
+    //                 tasker_other:['jim','john'],
+    //                 participators:['1','2','3'],
+    //                 publisher:'henry',
+    //                 ddl:'1288323623006',
+    //                 finishtime:'',
+    //                 base_score:'5',
+    //                 upvoters:['jim','小华'],
+    //                 status:0
+    //             },
+    //             {
+    //                 id:3,
+    //                 title:'task2',
+    //                 urgency:'正常',
+    //                 remark:'',
+    //                 tasker_main:'12',
+    //                 tasker_other:['小李','小王'],
+    //                 participators:['1','2','3'],
+    //                 publisher:'小华',
+    //                 ddl:'1289355623006',
+    //                 finishtime:'1288323623006',
+    //                 base_score:3,
+    //                 upvoters:[],
+    //                 status:2
+    //             },
+    //             {
+    //                 id:4,
+    //                 title:'紧急会议',
+    //                 urgency:'紧急',
+    //                 remark:'some word here',
+    //                 tasker_main:'henry',
+    //                 tasker_other:['smith','harry'],
+    //                 participators:['1','2','3'],
+    //                 publisher:'小明',
+    //                 ddl:'1461180991000',
+    //                 finishtime:'',
+    //                 base_score:2,
+    //                 upvoters:['henry'],
+    //                 status:0
+    //             }
+    //         ]
+    //     },
+    //     {
+    //         groupname:'技术任务',
+    //         index:1,
+    //         tasks:[
+    //             {
+    //                 id:5,
+    //                 title:'task1',
+    //                 urgency:'火急',
+    //                 remark:'some word here',
+    //                 tasker_main:'henry',
+    //                 tasker_other:['jim','john'],
+    //                 participators:['1','2','3'],
+    //                 publisher:'mary',
+    //                 ddl:'1285978923006',
+    //                 finishtime:'1288323623006',
+    //                 base_score:'5',
+    //                 upvoters:['jim','小华'],
+    //                 status:1
+    //             }
+    //         ]
+    //     }
+    // ]
 }]);
--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -7,20 +7,20 @@ app.controller("ctrl1",function($scope,$rootScope) {
 
 app.controller("ctrl_header",function($scope,$rootScope,$location,$http) {
     /*获取用户基本信息*/
-    // $http({
-    //     url: 'api/userinfo',
-    //     method: 'get',
-    //     params: {}
-    // }).success(function (data) {
-    //     $rootScope.userinfo=data;
-    // }).error(function () {
-    //     alert("获取信息失败，请稍后再试");
-    // });
+    $http({
+        url: 'api/userinfo',
+        method: 'get',
+        params: {}
+    }).success(function (data) {
+        $rootScope.userinfo=data;
+    }).error(function () {
+        alert("获取用户个人信息失败，请稍后再试");
+    });
 
     //TEMP
-    $rootScope.userinfo={
-        name:'小明'
-    };
+    // $rootScope.userinfo={
+    //     name:'小明'
+    // };
 
     $scope.logout=function () {
         //api logout
@@ -43,18 +43,19 @@ app.controller("ctrl_header",function($scope,$rootScope,$location,$http) {
 app.controller("ctrl_task",function($scope,$rootScope,$location,$http) {
     /*获取当前时间*/
     $scope.now=Date.parse( new Date());
+    //TEMP
     // console.log($scope.now);
 
     /*获取任务*/
-    // $http({
-    //     url: 'http://120.27.123.112:5001/sparkbition/api/task',
-    //     method: 'get',
-    //     params: {}
-    // }).success(function (data) {
-    //     $rootScope.groups=data;
-    // }).error(function () {
-    //     alert("获取信息失败，请稍后再试");
-    // });
+    $http({
+        url: 'api/task',
+        method: 'get',
+        params: {}
+    }).success(function (data) {
+        $rootScope.groups=data;
+    }).error(function () {
+        alert("获取任务信息失败，请稍后再试");
+    });
 
 
     //模型-正在查看的task
@@ -66,18 +67,53 @@ app.controller("ctrl_task",function($scope,$rootScope,$location,$http) {
         $scope.task_looking=this.task;
         $("#modal_task").modal();
     };
-    
-    
+
+
+    /*完成任务*/
     $scope.complete_task= function (task_id) {
         if (confirm("确定要完成这项任务吗？")) {
-            for(var i=0;i<$rootScope.groups.length;i++){
-                for(var j=0;j<$rootScope.groups[i].tasks.length;j++){
-                    if($rootScope.groups[i].tasks[j]['id'] ==task_id){
-                        $rootScope.groups[i].tasks[j]['status'] = 1;//把status改成1
-                        $rootScope.groups[i].tasks[j]['finishtime'] = Date.parse( new Date());//设置finishtime
+
+            $http({
+                url: 'api/complete_task',
+                method: 'get',
+                params: {task_id:task_id}
+            }).success(function (data) {
+                for(var i=0;i<$rootScope.groups.length;i++){
+                    for(var j=0;j<$rootScope.groups[i].tasks.length;j++){
+                        if($rootScope.groups[i].tasks[j]['id'] ==task_id){
+                            $rootScope.groups[i].tasks[j]['status'] = 1;//把status改成1
+                            $rootScope.groups[i].tasks[j]['finishtime'] = data.finishtime;//设置finishtime
+                        }
                     }
                 }
-            }
+            }).error(function () {
+                alert("获取信息失败，请稍后再试");
+            });
+
+        }
+    };
+
+
+    /*删除任务*/
+    $scope.delete_task= function (task_id) {
+        if (confirm("确定要删除这项任务吗？")) {
+
+            $http({
+                url: 'api/delete_task',
+                method: 'get',
+                params: {task_id:task_id}
+            }).success(function (data) {
+                for(var i=0;i<$rootScope.groups.length;i++){
+                    for(var j=0;j<$rootScope.groups[i].tasks.length;j++){
+                        if($rootScope.groups[i].tasks[j]['id'] ==task_id){
+                            $rootScope.groups[i].tasks.splice(j)//删除该task
+                        }
+                    }
+                }
+            }).error(function () {
+                alert("获取信息失败，请稍后再试");
+            });
+
         }
     };
     
@@ -96,7 +132,6 @@ app.controller("ctrl_newtask",function($scope,$rootScope,$location,$http) {
         'tasker_other':[],
         'participator':[]
     };
-    $rootScope.newtask.group=$rootScope.groups[0];
 
     //获取分组列表
     // $http({
@@ -109,6 +144,10 @@ app.controller("ctrl_newtask",function($scope,$rootScope,$location,$http) {
     //     alert("获取分组列表失败，请稍后再试");
     // });
     $rootScope.group_list=['主要任务','技术任务'];
+    $rootScope.newtask.group=$rootScope.group_list[0];
+
+    $rootScope.newtask.urgency="正常";
+
     //获取全部成员列表
     // $http({
     //     url: 'api/crew_list',
@@ -119,7 +158,8 @@ app.controller("ctrl_newtask",function($scope,$rootScope,$location,$http) {
     // }).error(function () {
     //     alert("获取成员列表失败，请稍后再试");
     // });
-    $rootScope.crew_list=['小明','test','hh'];
+    $rootScope.crew_list=['小明','test','hh','AA'];
+    $rootScope.newtask.tasker_main=$rootScope.userinfo.username;
 
     // // 设置默认的option
     // $rootScope.newtask={
@@ -147,4 +187,18 @@ app.controller("ctrl_newtask",function($scope,$rootScope,$location,$http) {
         $rootScope.newtask.participator.remove(crew);
     };
     
+    $scope.new_task= function () {
+        console.log(JSON.stringify($rootScope.newtask));
+        $http({
+            url: 'api/new_task',
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            data: JSON.stringify($rootScope.newtask)
+        }).success(function () {
+            alert("添加任务成功");
+            location.hash='#/task';
+        }).error(function () {
+            alert("获取信息失败，请稍后再试");
+        });
+    }
 });
