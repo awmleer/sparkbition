d436787cbb18d783b939196b3b41ec1226d45c1e
qzhsjz <qzhsjz@gmail.com>
2016-05-28 09:12:18 +0800

    任务关闭时给人发短信，以及改了任务关闭的时间。

--- backend/task_close.py
+++ backend/task_close.py
@@ -15,15 +15,32 @@ from flask.ext.cors import CORS      #跨域访问
 app = Flask(__name__)
 CORS(app)   #跨域访问
 
-#登录及用户认证
+def sendsms3(title, how, tasker_main, person, mobile):
+    d = {'#title#': title, '#how#': how, '#tasker_main#': tasker_main}
+    tpl_value = urllib.urlencode(d)
+    finalstr = ''
+    getdata = urllib.urlencode(
+        {'mobile': mobile, 'tpl_id': 13214, 'tpl_value': tpl_value, 'key': 'b32c625ffb38e4ad07f86bb1101548e1'})
+    url = 'http://v.juhe.cn/sms/send?%s' % getdata
+    req = urllib.urlopen(url)
+    result = json.loads(req.read())
+    finalstr += '发送给%s的短信的发送结果：%s\n' % (person, result['reason'].encode('utf-8'))
+    return finalstr
+
+#数据库连接
 client = MongoClient('120.27.123.112', 27017)
 client.admin.authenticate('fqs', '123456', mechanism='MONGODB-CR')
 uri = "mongodb://fqs:123456@120.27.123.112/admin?authMechanism=MONGODB-CR"
 client = MongoClient(uri)
 db=client.sparkbition
+tasks_file=db['tasks']
+users_db = db['users']
 while True:
-    tasks_file=db['tasks']
     for i in tasks_file.find({'status':1}):
-        if 1000*time.time()-i['finishtime']>259200000:
+        if 1000*time.time()-i['finishtime']>604800000:
             tasks_file.update({'id':i['id']},{'$set':{'status':2}})
+            task_score = int(i['base_score']) * (1 + 0.1 * len(i['upvoters']))
+            sendsms3(i['title'],"已经超过评审期并关闭，您的最终得分为" + str(task_score),i['tasker_main'],i['tasker_main'],users_db.find_one({'username': i['tasker_main']})['mobile']) #主负责人
+            for tasker in i['task_other']:
+                sendsms3(i['title'], "已经超过评审期并关闭，您的最终得分为" + str(task_score * 0.5), i['tasker_main'], tasker, users_db.find_one({'username': tasker})['mobile'])  # 其他负责人
     time.sleep(1200)
