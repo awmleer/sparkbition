1877f85dd6ffcf6a1cf8769b3a091976660df7d9
qzhsjz <qzhsjz@gmail.com>
2016-06-03 15:01:40 +0800

    import markdown angular

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', ['ui.router','chart.js']);
+var app = angular.module('myApp', ['ui.router','chart.js', 'hc.marked']);
 
 
 app.config(function($stateProvider, $urlRouterProvider){
